e1df7c27927776d9130ebcb9a204d8421e5a253c
iMAC <imac@iMac-de-iMAC.local>
2021-04-22 12:31:14 -0500

    Fix aplication

--- www/polyfills-es2015.js
+++ www/polyfills-es2015.js
@@ -203,8 +203,8 @@ module.exports = function (it, TAG, STATIC) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\rider\Documents\Desarrollos Independientes\Publibike\PublibikeBienestar-APP\PublibikeBienestarApp\PublibikeBienestarApp2\src\polyfills.ts */"hN/g");
-module.exports = __webpack_require__(/*! C:\Users\rider\Documents\Desarrollos Independientes\Publibike\PublibikeBienestar-APP\PublibikeBienestarApp\PublibikeBienestarApp2\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"LEsg");
+__webpack_require__(/*! /Users/imac/Desktop/master_publibike/src/polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! /Users/imac/Desktop/master_publibike/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"LEsg");
 
 
 /***/ }),
