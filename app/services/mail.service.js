40f55bc7b43684528ab0fc9717a0415467563d14
Johan Burgos <johan.burgos@evolutecc.co>
2022-09-09 15:10:24 -0500

    update mail

--- app/services/mail.service.js
+++ app/services/mail.service.js
@@ -15,7 +15,7 @@ module.exports.sendPasswordReset = (to, data) => {
     sgMail.send({
         to: to,
         from: 'noreply@uflou.com.co',
-        templateId: 'd-113e3fdd8b9e4916b7227670f2dde73f',
+        templateId: 'd-12ac3d4e3992424c94220b6ca09b5fa6',
         dynamicTemplateData: data
     }).then((response) => { console.log(response[0].statusCode); console.log(response[0].headers); }).catch((error) => { console.error(error) })
 }
